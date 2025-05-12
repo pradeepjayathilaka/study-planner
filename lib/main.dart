6c2103b71d1cf93a72c16f727e5c5765e7d912b1
pradeepjayathilaka <pradeepjayathilaka06@gmail.com>
2025-05-12 14:57:01 +0530

    Add Windows runner files for Flutter application
    - Created runner.exe.manifest to define application settings and DPI awareness.
    - Implemented utils.cpp and utils.h for console creation and command line argument handling.
    - Developed win32_window.cpp and win32_window.h to manage high DPI-aware Win32 windows, including theme updates and message handling.

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Flutter Demo')),
+        body: const Center(child: Text('Hello World')),
+      ),
+    );
+  }
+}
